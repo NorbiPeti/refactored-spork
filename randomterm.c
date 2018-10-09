dc1f556682b338fc259e0c4069f03dd4c504c69e
NorbiPeti <szatmari.norbert.peter@gmail.com>
2018-10-09 15:58:33 +0200

    Removed dep from randomterm

--- randomterm.c
+++ randomterm.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-#include "terminal-grafika.h"
+//#include "terminal-grafika.h"
 #include <stdlib.h>
 #include <time.h>
 
 int main() { //gcc -lm -Wall -o randomterm terminal-grafika.c randomterm.c
 	srand(time(NULL));
-	screen_t scr = new_screen(0, 20, 0, 20);
-	for(int i=0; i<20; i++)
-		screen_move_xy(scr, rand()%20, rand()%20);
+	printf("\033[%d;%dH", rand()%24 + 1, rand()%80 + 1);
 	return 0;
 }
 
